74ec38478284cc2e708af3fd1f37b94c6f1a1896
mohammad mehdi rashidi <mehdirashidi530@gmail.com>
2025-05-06 15:49:35 +0330

    notes_app done
    it works with a simple txt file
    whenever free
    jsonify it

--- notes_app.py
+++ notes_app.py
@@ -1,19 +1,98 @@
 import datetime as time
-txtfile = open("notes.txt","w")
-jsonfile = open("notes.json")
+import os
+
 def create_note():
-    title = input("enter the title of your new note: \n")
-    body = input("enter the body of your new note:\n")
-    date_of_creation = time.time()
-    txtfile.writelines(title + "\n")
-    txtfile.writelines(body + "\n")
-    #txtfile.writelines(date_of_creation)
+    txtfile = open("notes.txt",'a')
+    textfile = {"title" : str() , "body": list() , "creation_time" : str()}
+    title_name_input = input("\nenter your new notes title:\n")
+    if title_name_input in show_all_notes():
+        print("this title exists try another on!\n")
+        return
+    textfile["title"] = title_name_input
+    strinput = str()
+    bodyline = 0
+    while strinput != "finish body":
+        bodyline +=1
+        strinput = input(f"\nenter line {bodyline} and if done write finish body\n")
+        if strinput != "finish body":
+            textfile["body"].append(strinput)
+    textfile["creation_time"] = str(time.datetime.now())
+    txtfile.write("started title " + textfile["title"]+"\n")
+    for i in textfile["body"]:
+        txtfile.write(i + "\n")
+    txtfile.write(textfile["creation_time"] + "\n")
+    txtfile.write(f"finished title {textfile['title']}\n")
+    txtfile.close()
+
 def show_all_notes():
-    return
+    all_notes_titles = list()
+    txtfile = open("notes.txt" , 'r')
+    for line in txtfile.readlines():
+        if line[0:13] == "started title":
+            print(line[14:-2])
+            all_notes_titles.append(line[14:-2])
+    return all_notes_titles
 def search_in_notes():
-    return
+    txtfile = open("notes.txt" , 'r')
+    target_title = input("enter the title you want:\n")
+    existance = 0
+    for line in txtfile.readlines():
+        if line[0:13] == "started title":
+            if(line[14:]) == target_title+"\n":
+                existance = 1
+                print("your note:\n")
+                print(line[14:])
+                continue
+        elif existance == 1:
+            if line[0:14] == "finished title":
+                existance = 2
+                break
+            else:
+                print(line)
+    if existance == 0:
+        print("subject not found")
+    elif existance == 2:
+        print("well done!")
+
+        
+
+
 def delete_note():
-    return
+
+    txtfile = open("notes.txt" , 'r')
+    target_title = input("enter the title you want to delete:\n")
+    existance = 0
+    in_and_out_of_note = False
+    temptxt = open("temp.txt",'a')
+    for line in txtfile.readlines():
+        if line[0:13] == "started title":
+            in_and_out_of_note = True
+            if(line[14:]) == target_title+"\n":
+                existance = 1
+                continue
+            else:
+                temptxt.write(line)
+                continue
+        
+        if existance == 1:
+            if line[0:14] == "finished title":
+                in_and_out_of_note = False
+                existance = 2
+                continue
+            continue
+        if in_and_out_of_note:
+            temptxt.write(line)
+        if line[0:14] == "finished title":
+            in_and_out_of_note = False
+
+    txtfile.close()
+    temptxt.close()
+    if existance == 0:
+        print("subject not found")
+    elif existance == 2:
+        os.replace("temp.txt" , "notes.txt")
+        print("well done!")
+
 def exit_program():
     exit()
 
